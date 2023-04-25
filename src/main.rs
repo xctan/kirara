12add3ccd4026ab1ba03d0114f31ea40e0954832
twilight <t@bupt.edu.cn>
2023-04-25 17:13:20 +0800

    lex: integer const

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,54 @@
 use nom::{
-    IResult, combinator::recognize, character::complete::{one_of, digit0},
-    sequence::pair,
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{digit0, one_of},
+    combinator::recognize,
+    multi::many1,
+    sequence::{pair, preceded},
+    IResult,
 };
 
 use std::io::stdin;
+
+enum Token<'a> {
+    IntegerConst(&'a str),
+}
 
 fn main() {
     let mut input = String::new();
     stdin().read_line(&mut input).unwrap();
 
-    let num = decimal_const(input.as_str());
+    let num = integer_const(input.as_str());
+    let (_, num) = num.unwrap();
 
-    println!("  mv a1, {}", num.unwrap().1);
+    match num {
+        Token::IntegerConst(num) => println!("  mov a1, {}", num),
+    }
     println!("  ret");
 }
 
 fn decimal_const(input: &str) -> IResult<&str, &str> {
-    recognize(
-        pair(
-            one_of("123456789"), // non-zero digit
-            digit0,              // zero or more digits
-        )
-    )(input)
+    recognize(pair(
+        one_of("123456789"), // non-zero digit
+        digit0,              // zero or more digits
+    ))(input)
+}
+
+fn octal_const(input: &str) -> IResult<&str, &str> {
+    recognize(many1(one_of("01234567")))(input)
+}
+
+fn hexadecimal_const(input: &str) -> IResult<&str, &str> {
+    recognize(pair(
+        alt((tag("0x"), tag("0X"))),
+        recognize(many1(one_of("0123456789abcdefABCDEF"))),
+    ))(input)
+}
+
+fn integer_const(input: &str) -> IResult<&str, Token> {
+    alt((
+        hexadecimal_const,
+        octal_const,
+        decimal_const,
+    ))(input).map(|(i, o)| (i, Token::IntegerConst(o)))
 }
